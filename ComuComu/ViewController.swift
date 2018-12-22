e751871a399fc73a63b2c2b36586f13b77930745
Sho <shoyukimiukanae@gmail.com>
2018-12-22 17:02:17 +0900

    Initial Commit

---
+++ ComuComu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComuComu
+//
+//  Created by 高野翔 on 2018/12/22.
+//  Copyright © 2018 高野翔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
